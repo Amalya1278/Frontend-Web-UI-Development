679cb8f4ba21d81f3898934c41ed0b92cc763f6d
Amalya1278 <138888323+Amalya1278@users.noreply.github.com>
2024-02-23 19:45:00 +0400

    Add files via upload

---
+++ Day one/Task 2/Change_text_content.js
@@ -0,0 +1,6 @@
+var changeButton=document.getElementById("changeButton");
+var textParagraph=document.getElementById("textParagraph");
+changeButton.addEventListener("click",function(){
+    textParagraph.textContent="Poxvec";
+
+});
