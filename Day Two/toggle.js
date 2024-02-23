a6da9247a9c08acfe924f1890be49ae12759a2cc
Amalya1278 <138888323+Amalya1278@users.noreply.github.com>
2024-02-23 20:40:48 +0400

    Delete Day Two/toggle.js

--- Day Two/toggle.js
+++
@@ -1,9 +0,0 @@
-function toggleText() {  /*function for toggling*/
-    var button = document.getElementById("toggleButton");
-    if (button.textContent === "Hello") {
-        button.textContent = "Goodbye";
-    } else {
-        
-        button.textContent = "Hello";
-    }
-}
