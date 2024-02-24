011ab75b50078adc8125d032eaf9a56bdf9b0043
Amalya1278 <138888323+Amalya1278@users.noreply.github.com>
2024-02-24 14:49:13 +0400

    Add files via upload

---
+++ Day Three/Task3/html_form.js
@@ -0,0 +1,26 @@
+
+    var form = document.getElementById("form");
+
+    form.addEventListener("submit", function(event) {
+        var nameInput = document.getElementById("Name");
+        var emailInput = document.getElementById("Email");
+
+        var nameError = document.getElementById("nameError");
+        var emailError = document.getElementById("emailError");
+
+        nameError.textContent = "";
+        emailError.textContent = "";
+
+        
+        if (nameInput.value.trim() === "") {
+            nameError.textContent = "Please enter your name";
+            event.preventDefault();
+        }
+
+        // Validate Email
+        var emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+        if (!emailRegex.test(emailInput.value)) {
+            emailError.textContent = "Plaease enter yout email";
+            event.preventDefault();
+        }
+    });
