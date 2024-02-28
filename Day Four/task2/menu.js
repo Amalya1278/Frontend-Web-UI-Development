58b9102390b5892c8ea2d93851ca6b335e389fb2
Amalya1278 <138888323+Amalya1278@users.noreply.github.com>
2024-02-28 23:40:50 +0400

    Add files via upload

---
+++ Day Four/task2/menu.js
@@ -0,0 +1,9 @@
+
+    const mobileMenuToggle = document.getElementById('mobile-menu');
+    const navigation = document.querySelector('.navigation');
+  
+    mobileMenuToggle.addEventListener('click', function () {
+      navigation.classList.toggle('show');
+    });
+  
+  
